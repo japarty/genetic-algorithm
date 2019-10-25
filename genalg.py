dd707197e47548bc29e84137d117850e7bb8f3b7
yetanotherautist <jakubpart@gmail.com>
2019-10-25 22:47:38 +0200

    v.0.1.1 ranking selection added

--- genalg.py
+++ genalg.py
@@ -1,41 +1,41 @@
 import random
 import math
 
-test=[1,1,1,1,1,1,1,1]
+test=[ord('K'),ord('O'),ord('G'),ord('N'),ord('I'),ord('T'),ord('Y'),ord('W'),ord('I'),ord('S'),ord('T'),ord('Y'),ord('K'),ord('A')]
 HOW_MANY_CHROMOSOMES=20
 HOW_MANY_GENES=len(test)
-CROSSING_CHANCE=0.5
-MUTATION_CHANCE=0.2
+CROSSING_CHANCE=0.8
+MUTATION_CHANCE=0.02
 
 def population(individuals_value,chromosomes_value):
     population=[]
     for i in range(individuals_value):
         population.append([])
         for q in range(chromosomes_value):
-            population[i].append(random.randint(0,1))
+            population[i].append(random.randint(ord('A'),ord('Z')))
     return population
 
-def ocena_przystosowania(population,test):
-    różnice=[0]*len(population)
+def fitness_evaluation(population,test):
+    differences=[0]*len(population)
     for i in range(len(population)):
         for q in range(len(population[i])):
-            różnice[i]+=int(math.fabs(test[q]-population[i][q]))
-    return min(różnice)
+            differences[i]+=int(math.fabs(test[q]-population[i][q]))
+    return min(differences)
 
-def suma_przystosowania(population,test):
-    różnice=[0]*len(population)
+def fitness_sum(population,test):
+    differences=[0]*len(population)
     for i in range(len(population)):
         for q in range(len(population[i])):
-            różnice[i]+=int(math.fabs(test[q]-population[i][q]))
-    return sum(różnice)
+            differences[i]+=int(math.fabs(test[q]-population[i][q]))
+    return sum(differences)
 
-def ruletka(population):
-    suma_różnic=suma_przystosowania(population,test)
+def roulette(population): #not working
+    suma_różnic=fitness_sum(population,test)
     kolo=[]
     for i in range(len(population)):
         if i==0: poprzednia=0
         else: poprzednia=kolo[i-1]
-        kolo.append(poprzednia+(ocena_przystosowania([population[i]],test)/suma_różnic))
+        kolo.append(poprzednia+(fitness_evaluation([population[i]],test)/suma_różnic))
     nowa_populacja=[]
     for i in population:
         loteria=random.uniform(0, 1)
@@ -45,21 +45,35 @@ def ruletka(population):
                 break
     return nowa_populacja
 
+def ranking(population):
+    tournament=[]
+    for individual in population:
+        tournament.append([population[random.randint(0,len(population)-1)],population[random.randint(0,len(population)-1)]])
+    new_population=[]
+    for battle in tournament:
+        if fitness_evaluation([battle[0]],test)<fitness_evaluation([battle[1]],test):
+            new_population.append(battle[0])
+        elif fitness_evaluation([battle[0]],test)>fitness_evaluation([battle[1]],test):
+            new_population.append(battle[1])
+        elif fitness_evaluation([battle[0]],test)==fitness_evaluation([battle[1]],test):
+            new_population.append(battle[random.randint(0,1)])
+    return new_population
+
 def crossing(population,CROSSING_CHANCE):
     for i in range(0,len(population),2): #problem z nieparzystymi?
         if random.uniform(0, 1)<=CROSSING_CHANCE:
             miejsce_ciecia=random.randint(1,len(population[i])-1)
-            temp_1=population[i][:miejsce_ciecia]
+            temp_1=population[i][miejsce_ciecia:]
             temp_2=population[i+1][miejsce_ciecia:]
             population[i]=population[i][:miejsce_ciecia]+temp_2
-            population[i+1]=temp_1+population[i+1][miejsce_ciecia:]
+            population[i+1]=population[i+1][:miejsce_ciecia]+temp_1
     return population
 
 def mutation(population,MUTATION_CHANCE):
     for individual in population:
         if random.uniform(0,1)<=MUTATION_CHANCE:
             mutating_genome=random.randint(0,len(individual)-1)
-            individual[mutating_genome]=random.randint(0,1)
+            individual[mutating_genome]=random.randint(ord('A'),ord('Z'))
     return population
 
 
@@ -67,18 +81,11 @@ population=population(HOW_MANY_CHROMOSOMES,HOW_MANY_GENES)
 print(population)
 loops=0
 
-while ocena_przystosowania(population,test)>0 and loops<=1000000:
+while fitness_evaluation(population,test)>0:
     loops+=1
-    if loops%100==0: print(ocena_przystosowania(population,test), loops)
-    population=ruletka(population)
+    if loops%100==0: print('i=',loops,fitness_evaluation(population,test))
+    population=ranking(population)
     population=crossing(population,CROSSING_CHANCE)
     population=mutation(population,MUTATION_CHANCE)
 print(loops)
 print(population)
-'''
-population=ruletka(population)
-print(population)
-population=crossing(population,0.8)
-print(population)
-population=mutation(population,0.8)
-'''
